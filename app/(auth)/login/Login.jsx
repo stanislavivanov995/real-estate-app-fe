cdc8ba05b0f917da58056beac0963c6ca5776207
Jykata <jykatabg@gmail.com>
2023-10-26 20:14:12 +0300

    Form visual update

--- app/(auth)/login/Login.jsx
+++ app/(auth)/login/Login.jsx
@@ -3,39 +3,38 @@ import PublicLayout from "@/components/layouts/PublicLayout";
 import Image from "next/image.js";
 
 export default function Login() {
-  // TODO: The users must be able to login with Email and Password
   return (
     <PublicLayout>
-      <div className="flex justify-center mt-[3em]">
-        <div className="w-[720px] h-[824px] bg-slate-50 flex-col justify-start items-center inline-flex shadow-2xl rounded-tl-[3em] rounded-bl-[3em]">
+      <div className="flex justify-center mt-[5em]">
+        <div className="w-[620px] sm:h-[800px] h-[840px] bg-slate-50 flex-col justify-start items-center inline-flex shadow-2xl rounded-tl-[3em] rounded-bl-[3em]">
           <div className="flex flex-col justify-center gap-5 my-[100px]">
             <div className="flex flex-col align-middle justify-center text-center sm:text-left">
-              <h1 className="sm:text-[40px] text-[25px] font-bold leading-normal">
+              <h1 className="sm:text-[35px] text-[25px] font-bold leading-normal">
                 Login to your Account
               </h1>
-              <h3 className=" text-zinc-500 text-[18px] sm:text-xl font-normal">
+              <h3 className=" text-zinc-500 text-[18px] sm:text-[16px] font-normal">
                 Welcome back! Select method to log in:
               </h3>
             </div>
             <div className="sm:flex-row flex-col items-center flex sm:justify-between align-middle gap-[40px]">
               <div className="flex align-middle justify-center gap-2">
-                <button className=" flex justify-center items-center w-[215px] h-[60px] gap-5 border-2 border-indigo-400 text-[22px] font-medium text-zinc-950 border-opacity-50 rounded-[10px]">
+                <button className=" flex justify-center items-center sm:w-[180px] w-[215px] h-[50px] gap-5 border-2 border-indigo-400 text-[18px] font-medium text-zinc-950 border-opacity-50 rounded-[10px]">
                   <Image
                     src="/logos/Google.png"
                     alt="google logo"
-                    width={30}
-                    height={30}
+                    width={20}
+                    height={20}
                   />
                   Google
                 </button>
               </div>
               <div>
-                <button className=" flex justify-center items-center w-[215px] h-[60px] gap-5 border-2 border-indigo-400 text-[22px] font-medium text-zinc-950 border-opacity-50 rounded-[10px]">
+                <button className=" flex justify-center items-center sm:w-[180px] w-[215px] h-[50px] gap-5 border-2 border-indigo-400 text-[18px] font-medium text-zinc-950 border-opacity-50 rounded-[10px]">
                   <Image
                     src="/logos/Facebook.png"
                     alt="facebook logo"
-                    width={30}
-                    height={30}
+                    width={25}
+                    height={25}
                   />
                   Facebook
                 </button>
@@ -57,13 +56,13 @@ export default function Login() {
             </p>
           </div>
         </div>
-        <div className="bg-slate-700  w-[720px] h-[824px] shadow-2xl rounded-tr-[3em] rounded-br-[3em] hidden sm:block">
+        <div className="bg-slate-700  w-[620px] h-[800px] shadow-2xl rounded-tr-[3em] rounded-br-[3em] hidden sm:block">
           <div className=" flex flex-col items-center gap-[3em] justify-center mt-[100px]">
             <Image
               className="flex"
               src="/login image.png"
               alt="register"
-              width={550}
+              width={500}
               height={200}
             />
             <div className="text-center">
--- components/layouts/Login/LoginForm.jsx
+++ components/layouts/Login/LoginForm.jsx
@@ -2,6 +2,7 @@
 
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import Image from "next/image.js";
+import * as Yup from "yup";
 
 const initialValues = {
   email: "",
@@ -9,16 +10,27 @@ const initialValues = {
   toggle: false,
 };
 
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required!")
+    .email("Email must be valid"),
+  password: Yup.string().required("Password is required!"),
+});
+
 function handleFunction(data) {
   console.log(Object.values(data));
 }
 
 export default function LoginForm() {
   return (
-    <Formik initialValues={initialValues} onSubmit={handleFunction}>
-      <Form className=" mt-[25px] items-center flex flex-col gap-[15px] w-[450px]">
-        <div className="w-[350px] sm:w-[450px] h-16 bg-indigo-400 bg-opacity-10 rounded-[10px] border-2 border-indigo-400 flex justify-start text-lg items-center pl-2 gap-2.5">
-          <Image src={"/email.png"} alt="password" width={30} height={30} />
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleFunction}
+      validationSchema={validationSchema}
+    >
+      <Form className=" mt-[25px] items-center flex flex-col gap-[10px] w-[450px]">
+        <div className="w-[350px] sm:w-[450px] mb-4 h-12 bg-indigo-400 bg-opacity-10 rounded-[10px] border-2 border-indigo-400 flex justify-start text-lg items-center pl-2 gap-2.5">
+          <Image src={"/email.png"} alt="password" width={25} height={25} />
           <Field
             placeholder="Email"
             name="email"
@@ -26,13 +38,18 @@ export default function LoginForm() {
             className="bg-transparent outline-none w- p-1"
           />
         </div>
-        <div className="w-[350px] sm:w-[450px] h-16 bg-indigo-400 bg-opacity-10 rounded-[10px] border-2 border-indigo-400 flex justify-between text-lg items-center pl-2 gap-2.5">
+        <ErrorMessage
+          name="email"
+          component="span"
+          className=" text-red-600 mb-3"
+        />
+        <div className="w-[350px] sm:w-[450px] mb-4 h-12 bg-indigo-400 bg-opacity-10 rounded-[10px] border-2 border-indigo-400 flex justify-between text-lg items-center pl-2 gap-2.5">
           <div className=" flex gap-2">
             <Image
               src={"/shield-slash.png"}
               alt="password"
-              width={30}
-              height={30}
+              width={25}
+              height={25}
             />
             <Field
               placeholder="Password"
@@ -46,22 +63,29 @@ export default function LoginForm() {
             <i className="fa-solid fa-eye-slash"></i>
           </div>
         </div>
-        <div className=" flex sm:flex-row flex-col sm:gap-[10em] gap-3 mb-[25px]">
-          <div className="flex gap-2">
+        <ErrorMessage
+          name="password"
+          component="span"
+          className=" text-red-600 mb-3"
+        />
+        <div className=" flex sm:flex-row flex-col sm:gap-[11em] gap-3 mb-[25px] mt-[1em]">
+          <div className="flex items-center gap-2">
             <Field
               type="checkbox"
               name="toggle"
-              className="border border-indigo-400 w-[20px]"
+              className="border border-indigo-400 w-[16px]"
             />
-            <label className="text-zinc-500 font-normal">Remember me</label>
+            <label className="text-zinc-500 font-normal text-[15px]">
+              Remember me
+            </label>
           </div>
-          <a href="#" className="text-indigo-400 mr-2">
+          <a href="#" className="text-indigo-400 mr-2 text-[15px]">
             Forgot Passowrd?
           </a>
         </div>
         <button
           type="submit"
-          className="sm:w-[453px] w-[340px] h-16 p-2.5 bg-blue-600 rounded-[10px] text-white text-xl font-bold mb-[20px]"
+          className="sm:w-[353px] w-[340px] h-12 p-2.5 bg-blue-600 rounded-[10px] text-white text-xl font-bold mb-[20px]"
         >
           LOG IN
         </button>
