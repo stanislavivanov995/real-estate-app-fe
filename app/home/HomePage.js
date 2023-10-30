fd0e8bf9eb82c4d55f21c3a184544aeaefc70e7d
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-10-30 15:27:55 +0200

    update home

--- app/home/HomePage.js
+++ app/home/HomePage.js
@@ -1,18 +1,35 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import PublicLayout from "@/components/layouts/PublicLayout";
 
 export default function HomePage() {
-    // TODO:
-    return (
-        <PublicLayout>
-            <h1 className={'h1-bold text-center'}>Homepage</h1>
+  const [posts, setPosts] = useState([]);
 
-            <div className="my-20 text-center">
-                <Link className={'px-8 py-2 border shadow rounded-md hover:bg-light-500 hover:text-light-100'}
-                      href={'/register'}>
-                    Get Started
-                </Link>
-            </div>
-        </PublicLayout>
-    );
-};
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <PublicLayout>
+      <h1 className="h1-bold text-center">Homepage</h1>
+
+      <div className="posts">
+        {posts.map((post) => (
+          <div key={post.id}>Post TI {post.title}</div>
+        ))}
+      </div>
+
+      <div className="my-20 text-center">
+        <Link
+          className="px-8 py-2 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
+          href="/register"
+        >
+          Get Started
+        </Link>
+      </div>
+    </PublicLayout>
+  );
+}
