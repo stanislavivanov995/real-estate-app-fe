652098f33ef1dc59042f7f7d14129cee8f678489
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-11-04 13:34:18 +0200

    fixing fields in create form

--- components/layouts/Create/CreateForm.jsx
+++ components/layouts/Create/CreateForm.jsx
@@ -54,6 +54,36 @@ export default function CreateForm() {
             placeholder="$2999"
           />
         </div>
+        <div className="w-full">
+          <label
+            htmlhtmlFor="rooms"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Rooms count
+          </label>
+          <input
+            type="number"
+            name="rooms"
+            id="rooms"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="2"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="location"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Location
+          </label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Property location"
+          />
+        </div>
         <div className="sm:col-span-2">
           <label
             htmlhtmlFor="description"
@@ -73,7 +103,7 @@ export default function CreateForm() {
         type="submit"
         className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
       >
-        Add product
+        Post
       </button>
     </form>
   );
