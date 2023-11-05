83b59921273a273fa54a0da1462cb5ade103ed20
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-11-05 12:54:01 +0200

    update

--- components/layouts/DashboardLayout/DashboardLayout.js
+++ components/layouts/DashboardLayout/DashboardLayout.js
@@ -1,5 +1,5 @@
 import Sidebar from "@/components/dashboard components/Sidebar/Sidebar";
-import Navbar from "@/components/dashboard components/Header/Header";
+import Header from "@/components/dashboard components/Header/Header";
 import Footer from "@/components/dashboard components/Footer/Footer";
 
 export default function DashboardLayout({ children }) {
@@ -7,7 +7,7 @@ export default function DashboardLayout({ children }) {
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex flex-col w-screen h-full">
-        <Navbar />
+        <Header />
         <div className="overflow-y-auto m-auto custom-scrollbar">
           {children}
         </div>
