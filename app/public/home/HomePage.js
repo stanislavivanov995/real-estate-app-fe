10e81c1c646ed6de5650c83fe33ca11c859e6316
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-11-09 22:27:31 +0200

    update homepage

--- app/public/home/HomePage.js
+++ app/public/home/HomePage.js
@@ -1,18 +1,36 @@
-import Link from "next/link";
-import PublicLayout from "@/components/layouts/PublicLayout";
+import SearchProperty from "@/app/public/home/SearchProperty";
+import TopRatedPropertyCard from "@/app/public/home/TopRatedPropertyCard";
 
 export default function HomePage() {
-    // TODO:
-    return (
-        <PublicLayout>
-            <h1 className={'h1-bold text-center'}>Homepage</h1>
 
-            <div className="my-20 text-center">
-                <Link className={'px-8 py-2 border shadow rounded-md hover:bg-light-500 hover:text-light-100'}
-                      href={'/register'}>
-                    Get Started
-                </Link>
-            </div>
-        </PublicLayout>
+    const topRatedProperties = [
+        {id:1, name: 'Property One', city: 'Plovdiv', imgUrl: '/assets/propertyone.png', alt: 'Property One Image'},
+        {id:2, name: 'Property Two', city: 'Sofia', imgUrl: '/assets/propertytwo.png', alt: 'Property Two Image'},
+        {id:2, name: 'Property Three', city: 'Blagoevgrad', imgUrl: '/assets/propertythree.png', alt: 'Property Three Image'},
+    ];
+
+    return (
+        <main className={'min-h-screen bg-gray-100'}>
+
+            <h1 className="py-10 text-center text-3xl font-bold tracking-tighter">Welcome to the best booking website.</h1>
+
+            {/* Search bar */}
+            <SearchProperty />
+
+            {/* Top-rated properties */}
+            <h3 className={'ml-12 mt-14 text-2xl font-semibold'} >Top-rated properties </h3>
+
+            <section className={'m-12 mt-5 grid grid-cols-1 gap-4 md:grid-cols-3'}>
+
+                {
+                    topRatedProperties.map(property =>
+                    <TopRatedPropertyCard key={property.id}
+                                          propertyName={property.name}
+                                          city={property.city} />)
+                }
+
+            </section>
+
+        </main>
     );
 };
