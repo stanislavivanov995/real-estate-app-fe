490dba3ee4bf9553664d70f2bc6ac300c9c228f4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-11-14 20:25:11 +0200

    created empty profile page

---
+++ app/(user)/profile/page.js
@@ -0,0 +1,9 @@
+import UserLayout from "@/components/layouts/UserLayout/UserLayout";
+
+export default function ProfilePage() {
+    return (
+        <UserLayout>
+            <h1 className="text-center text-4xl">Profile Page</h1>
+        </UserLayout>
+    );
+};
