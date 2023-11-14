d19e7a22038ba23e19d4b7d89b77398858d2c259
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-11-14 19:25:31 +0200

    Public Navigation improved!

--- components/layouts/PublicLayout/PublicLayout.js
+++ components/layouts/PublicLayout/PublicLayout.js
@@ -1,61 +1,48 @@
 import Link from "next/link";
 
-export default function PublicLayout({ children }) {
-  return (
-    <>
-      <header className={"shadow py-4"}>
-        <nav className={"flex-center gap-4"}>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"/"}
-          >
-            Home
-          </Link>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"/about"}
-          >
-            About
-          </Link>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"/faq"}
-          >
-            FAQ
-          </Link>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"/login"}
-          >
-            Login
-          </Link>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"/register"}
-          >
-            Register
-          </Link>
-          <Link
-            className={
-              "px-4 py-1 border shadow rounded-md hover:bg-light-500 hover:text-light-100"
-            }
-            href={"admin/dashboard"}
-          >
-            Dashboard
-          </Link>
-        </nav>
-      </header>
-      {children}
-    </>
-  );
+export default function PublicLayout({children}) {
+
+    const publicNavLinks = [
+        {href: '/', name: 'Home'},
+        {href: '/public/faq', name: 'FAQ'},
+        {href: '/public/about', name: 'About'},
+        {href: '/public/contacts', name: 'Contacts'},
+    ];
+
+    return (
+        <>
+            <header className={"flex items-center justify-between px-12 py-3 shadow border-b border-slate-300"}>
+
+                <div className={'flex gap-20'}>
+
+                    {/* LOGO */}
+                    <div>SITE LOGO</div>
+
+                    {/* Nav Links */}
+                    <nav className={"flex items-center gap-10"}>
+                        {
+                            publicNavLinks.map(link =>
+
+                                <Link key={link.href}
+                                    className={'text-[15px] font-medium text-blue-950 hover:text-blue-800 hover:underline'}
+                                    href={link.href}
+                                >
+                                    {link.name}
+                                </Link>
+                            )
+                        }
+                    </nav>
+
+                </div>
+
+                {/* Login Register Buttons */}
+                <div className={'flex items-center gap-4'}>
+                    <button className={'rounded-lg bg-blue-500 py-3 px-4 text-[15px] font-semibold text-white hover:bg-blue-600 hover:shadow-lg transition ease-in-out hover:scale-105 duration-300'}>Sign up</button>
+                    <button className={'rounded-lg bg-gray-200 py-3 px-4 text-[15px] font-semibold text-blue-950 hover:bg-gray-100 hover:shadow-lg transition ease-in-out hover:scale-105 duration-300'}>Login in</button>
+                </div>
+
+            </header>
+            {children}
+        </>
+    );
 }
