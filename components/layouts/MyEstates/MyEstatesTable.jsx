f682bf8208555931ba3d43b0cc5266772182925f
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2023-11-27 21:50:07 +0200

    update

--- components/layouts/MyEstates/MyEstatesTable.jsx
+++ components/layouts/MyEstates/MyEstatesTable.jsx
@@ -1,23 +1,23 @@
 import Link from "next/link.js";
 export default function MyEstatesTable({ entries }) {
   return (
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Property Name
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Location
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Category
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Price
             </th>
-            <th scope="col" class="px-12 py-3">
+            <th scope="col" className="px-12 py-3">
               Action
             </th>
           </tr>
@@ -26,40 +26,40 @@ export default function MyEstatesTable({ entries }) {
           {entries.map((estate) => (
             <tr
               key={estate.id}
-              class="odd:bg-white h-[5em] text-lg odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+              className="odd:bg-white h-[5em] text-lg odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
             >
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
                 {estate.propertyName}
               </th>
-              <td class="px-6 py-4">{estate.location}</td>
-              <td class="px-6 py-4">{estate.category}</td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">{estate.location}</td>
+              <td className="px-6 py-4">{estate.category}</td>
+              <td className="px-6 py-4">
                 {estate.price}
                 {estate.currency}
               </td>
-              <td class="px-6 py-4">
+              <td className="px-6 py-4">
                 <div className="flex items-center gap-5">
                   <button
                     href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                   >
-                    <i class="fa-solid fa-pencil"></i>
+                    <i className="fa-solid fa-pencil"></i>
                   </button>
                   <button
                     href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                   >
-                    <i class="fa-solid fa-trash"></i>
+                    <i className="fa-solid fa-trash"></i>
                   </button>
                   <Link href={""}>
                     <button
                       href="#"
-                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                     >
-                      <i class="fa-solid fa-eye"></i>
+                      <i className="fa-solid fa-eye"></i>
                     </button>
                   </Link>
                 </div>
